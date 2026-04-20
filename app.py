439b44e6c2555a3db6560b44485a42a4b088df89
neadahlen <neadahlen@gmail.com>
2026-04-20 09:11:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+#routes to webbpages
 @app.route("/")
 def index():
     return render_template("home.html")
 
 
+
+#runner and debugger
 if __name__ in "__main__":
     app.run(debug=True)
