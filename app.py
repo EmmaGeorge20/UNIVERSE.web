2df7cfc636a4a4f36280236ea8c9cc1a41ba16ae
Seniha <seniha.uni@gmail.com>
2026-04-21 22:05:06 +0200

    Fixade login Sidan och CSS

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 #runner and debugger
-if __name__ in "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
