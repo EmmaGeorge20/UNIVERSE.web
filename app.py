98279c10d0ba0cc8a5c5fd23ccb59da04df3c3cf
Seniha <seniha.uni@gmail.com>
2026-05-02 22:17:49 +0200

    Skapade startup sidan.
    Skapade start-up-sida

--- app.py
+++ app.py
@@ -19,5 +19,12 @@ def index():
     """
     return render_template("index.html")
 
+@app.route("/startup")
+def startup():
+    """
+    Renders the startup page.
+    """
+    return render_template("startup.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
